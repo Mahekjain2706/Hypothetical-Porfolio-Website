6d3bd96941aab64bec8f0606b83aa9a936d98a8e
Prajakta <prajakta.rd29@gmail.com>
2024-03-22 23:07:35 +0530

    Updated insertStocks

--- backend/tradeAnalyzer/core/utils/insertstocks.py
+++ backend/tradeAnalyzer/core/utils/insertstocks.py
@@ -3,7 +3,6 @@ import yfinance as yf
 def get_current_price(symbol):
     ticker = yf.Ticker(symbol)
     todays_data = ticker.history(period='1d')
-    print(todays_data)
     return todays_data['Close'][0]
 
 url = 'http://localhost:8000/addstocks/'
